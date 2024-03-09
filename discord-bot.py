c515983a5aa78a9f44cadfa6decdb4d134e2e93c
Nick McNeese <nickmcneese@berkeley.edu>
2024-03-09 01:12:01 -0800

    first commit

---
+++ discord-bot.py
@@ -0,0 +1,192 @@
+import discord
+import pymongo
+import re
+from discord.ext import commands
+from discord import Client, Intents
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = ""
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+db = client["Users"]
+currency_collection = db["currency"]
+ranking_collection = db["ranking"]
+
+
+BOT_TOKEN = ""
+MAX_USER_DAILY_RANKING_CURRENCY = 50
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+#want to add new discord server members to both databases
+@bot.event
+async def on_member_join(member):
+    #join rank DB
+    member = member.name
+    ranking_document = ranking_collection.find_one({"member": member})
+    if ranking_document:
+        print(f"The person already exists in the ranking document.")
+    else:
+        print(f"The person does not exist in the ranking document. Inserting...")
+        data = {
+            "member": member,
+            "ranking": 0,
+            "ranking_delta": 0,
+        }
+        ranking_collection.insert_one(data)
+    
+    #join currency DB
+    currency_document = currency_collection.find_one({"author": member})
+    if currency_document:
+        print(f"The person already exists in the document!")
+    else:
+        print(f"The person does not exist in the document. Inserting...")
+        data = {
+            "author": member,
+            "amount_positive": MAX_USER_DAILY_RANKING_CURRENCY,
+            "amount_negative": MAX_USER_DAILY_RANKING_CURRENCY,
+        }
+        currency_collection.insert_one(data)
+
+def find_member(guild, username):
+    # Search for the member by username or user ID
+    member = discord.utils.find(
+        lambda m: username.lower() in m.name.lower() or str(m.id) == username,
+        guild.members
+    )
+    return member
+
+@bot.command()
+async def rank(ctx, username):
+    member = find_member(ctx.guild, username).name
+    ranking_document = ranking_collection.find_one({"member": member})
+    delta = ranking_document["ranking_delta"]
+    if ranking_document:
+        rank_emoji = None
+        if delta == 1:
+            rank_emoji = ":chart_with_upwards_trend:"
+        else:
+            rank_emoji = ":chart_with_downwards_trend:"
+        await ctx.send("Current rank: " + str(ranking_document["ranking"]) + " " + rank_emoji)
+    else:
+        await ctx.send("Error. This person is not in the server.")
+    
+def parse_input(input_string):
+    pattern = r'^[+-]\d+$'
+
+    if re.match(pattern, input_string):
+        # Valid format
+        sign = input_string[0]
+        number = int(input_string[1:])
+        return sign, number
+    else:
+        # Invalid format
+        return "Error"
+
+@bot.command()
+async def stonks(ctx, username, input):
+    #caller of command by unique discord username
+    author = ctx.author.name
+
+    #parse input
+    result = parse_input(input)
+    if result == "Error":
+        await ctx.send("Error, wrong format!")
+        return
+    sign, integer = result
+
+    #check if user exists
+    member = find_member(ctx.guild, username).name
+
+    #make sure you can't rate yourself
+    if member == author:
+        await ctx.send("You cannot give rating to yourself!")
+        return
+
+    #check if author is in the currency table. if they are not, add them.
+    currency_document = currency_collection.find_one({"author": author})
+    if currency_document:
+        print(f"The person already exists in the document.")
+    else:
+        print(f"The person does not exist in the document. Inserting...")
+        data = {
+            "author": author,
+            "amount_positive": MAX_USER_DAILY_RANKING_CURRENCY,
+            "amount_negative": MAX_USER_DAILY_RANKING_CURRENCY,
+        }
+        currency_collection.insert_one(data)
+    
+    #Check if user exceeds their daily limit. If so, give error. Else, subtract.
+    entry = currency_collection.find_one({"author": author})
+    amount_positive = entry["amount_positive"]
+    amount_negative = entry["amount_negative"]
+    if sign == "+":
+        if amount_positive - integer >= 0:
+            new_value = {"$set": {"amount_positive" : amount_positive - integer}}
+            currency_collection.update_one(entry, new_value)
+        else:
+            await ctx.send("Not enough daily currency! Positive amount remaining: " + str(amount_positive) + ". Negative amount remaining: " + str(amount_negative))
+            return
+    elif sign == "-":
+        if amount_negative - integer >= 0:
+            new_value = {"$set": {"amount_negative" : amount_negative - integer}}
+            currency_collection.update_one(entry, new_value)
+        else:
+            await ctx.send("Not enough daily currency! Positive amount remaining: " + str(amount_positive) + ". Negative amount remaining: " + str(amount_negative))
+            return
+    else:
+        #will want to throw error
+        print("there was an error")
+
+    #Update ranking for the member specified based on amount
+    #First have to check if member is in the ranking DB, otherwise add them
+    ranking_document = ranking_collection.find_one({"member": member})
+    if ranking_document:
+        print(f"The person already exists in the ranking document.")
+    else:
+        print(f"The person does not exist in the ranking document. Inserting...")
+        data = {
+            "member": member,
+            "ranking": 0,
+            "ranking_delta": 0
+        }
+        ranking_collection.insert_one(data)
+    
+    #Now we update their ranking
+    entry = ranking_collection.find_one({"member": member})
+    current_ranking = entry["ranking"]
+    rank_emoji = None
+    if sign == "+":
+        new_value = {"$set": { "ranking": current_ranking + integer, "ranking_delta": 1 }}
+        ranking_collection.update_one(entry, new_value)
+        rank_emoji = ":chart_with_upwards_trend:"
+    elif sign == "-":
+        new_value = {"$set": { "ranking": current_ranking - integer, "ranking_delta": -1 }}
+        ranking_collection.update_one(entry, new_value)
+        rank_emoji = ":chart_with_downwards_trend:"
+    else:
+        await ctx.send("Invalid Format")
+
+    new_rank = ranking_collection.find_one({"member": member})["ranking"]
+    await ctx.send("Updated rank for " + member + "! New rank is: " + str(new_rank) + " " + rank_emoji)
+
+@stonks.error
+async def stonks_error(ctx, error):
+    if isinstance(error, commands.errors.UserNotFound):
+        await ctx.send("User not on server")
+
+@bot.command()
+async def currency(ctx):
+    author = ctx.author.name
+    currency_document = currency_collection.find_one({"author": author})
+    if currency_document:
+        amount_positive = currency_document["amount_positive"]
+        amount_negative = currency_document["amount_negative"]
+        await ctx.send("You have +" + str(amount_positive) + " and -" + str(amount_negative) + " left to spend for today!")
+        return
+    else:
+        await ctx.send("You are not yet in the system. Use the [!stonks <user> <+/-><number>] command to register!")
+        return
+ 
+bot.run(BOT_TOKEN)
